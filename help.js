b985efc0375712f3a04838b5566d428de0db43eb
AlphaRacer <tarun.a1441@gmail.com>
2021-02-25 18:50:57 +0530

    Add help command

---
+++ help.js
@@ -0,0 +1,15 @@
+module.exports = {
+    name: 'help',
+    description: 'help command',
+    execute(message, args) {
+        if (!args) {
+            message.reply("Please specify the topic on which you want help.\n\nDo `!help server` for help with the server, do `!help bot` for help with the bot.");
+        }
+        if (args[0] === "server") {
+            message.reply("Work in progress...");
+        }
+        else if (args[1] === "bot") {
+            message.reply("Work in progress...");
+        }
+    }
+}
