5943b27aa17935212613c5fa60f15697b36ebb15
AlphaRacer <tarun.a1441@gmail.com>
2021-02-25 17:13:56 +0530

    Add latency info to ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'ping',
     description: 'Ping!',
     execute(message, args) {
-        message.channel.send('Pong.');
+        message.channel.send(`Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
     },
 };
