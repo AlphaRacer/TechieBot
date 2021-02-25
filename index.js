5ca396a26daf5b27d58e0f0e09c3fb6102b80392
AlphaRacer <tarun.a1441@gmail.com>
2021-02-25 16:52:31 +0530

    Add command handler

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 require('dotenv').config()
-
+const { prefix } = require('./config.json');
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-        message.reply('Pong!');
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).trim().split(' ');
+    const commandName = args.shift().toLowerCase();
+
+    if (!client.commands.has(commandName)) return;
+
+    const command = client.commands.get(commandName);
+
+    for (const folder of commandFolders) {
+        const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
+        for (const file of commandFiles) {
+            const command = require(`./commands/${folder}/${file}`);
+            client.commands.set(command.name, command);
+        }
     }
+
+
+    try {
+        command.execute(message, args);
+    } catch (error) {
+        console.error(error);
+        message.reply('there was an error trying to execute that command!');
+    }
+
 });
 
 client.login(process.env.TOKEN);
