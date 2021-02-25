cc28d8032aff410f5f5214d8c11a5e4ad40e6a65
AlphaRacer <tarun.a1441@gmail.com>
2021-02-25 17:14:53 +0530

    fix ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'ping',
     description: 'Ping!',
     execute(message, args) {
-        message.channel.send(`Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
+        message.channel.send(`Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(message.client.ws.ping)}ms`);
     },
 };
