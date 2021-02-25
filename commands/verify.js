18ca78cb0b8e2dc3f323db0c2dcc9f2e739ad55d
AlphaRacer <tarun.a1441@gmail.com>
2021-02-25 18:40:09 +0530

    Change verify message to welcome user

--- commands/verify.js
+++ commands/verify.js
@@ -3,8 +3,8 @@ module.exports = {
     description: 'verify command',
     execute(message, args) {
         try {
-            message.channel.send(`${message.author.tag} has verified!`);
             message.member.roles.add('814434340452827216');
+            message.client.channels.cache.get('814459282288279552').send(`${message.author}, welcome to the server!`);
         }
         catch (error) {
             console.error(error);
