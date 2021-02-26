87787137f290ec3af5556316f1624ac91174a2a1
AlphaRacer <tarun.a1441@gmail.com>
2021-02-26 11:17:04 +0530

    try to fix bug

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,8 @@
 module.exports = {
-    name: 'ping',
-    description: 'Ping!',
-    execute(message, args) {
-        message.channel.send(`Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(message.client.ws.ping)}ms`);
-    },
+	name: 'ping',
+	description: 'Ping!',
+	// eslint-disable-next-line no-unused-vars
+	execute(message, args) {
+		message.channel.send(`Pong! Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(message.client.ws.ping)}ms`);
+	},
 };
