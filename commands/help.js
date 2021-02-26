36fc246889faf9e1212d299c3ffde161f9f094ad
AlphaRacer <tarun.a1441@gmail.com>
2021-02-26 09:04:08 +0530

    Make help messages embeds

--- commands/help.js
+++ commands/help.js
@@ -5,7 +5,6 @@ module.exports = {
 		const Discord = require('discord.js');
 		if (!args[0]) {
 			const wrongUsageEmbed = new Discord.MessageEmbed()
-				.setDescription('Please specify the topic on which you want help.')
 				.setColor('#17a2b8')
 				.addField('Server Help', 'Send `!help server`')
 				.addField('Bot Help', 'Do `!help bot`');
@@ -14,17 +13,29 @@ module.exports = {
 		}
 		if (args[0] === 'server') {
 			if (!args[1]) {
-				message.channel.send('Possible help options:\n\n> 1. **Where do I ask my tech question?**, send "`!help server 1`"\n> 2. **Where do I get general tech support?**, send "`!help server 2`"');
+				const serverHelpEmbed = new Discord.MessageEmbed()
+					.setColor('#17a2b8')
+					.setTitle('Server Help')
+					.addField('Where do I ask my tech question?', 'Send `!help server 1`')
+					.addField('Where do I get general tech support?', 'Send `!help server 2`');
+				// message.channel.send('Possible help options:\n\n> 1. **Where do I ask my tech question?**, send "`!help server 1`"\n> 2. **Where do I get general tech support?**, send "`!help server 2`"');
+				message.channel.send(serverHelpEmbed);
 			}
 			else if (args[1] === '1') {
-				message.channel.send('Ask your tech question in <#814459285546991676>.');
+				message.reply('Ask your tech question in <#814459285546991676>.');
 			}
 			else if (args[1] === '2') {
-				message.channel.send('Get general tech support in <#814459286460563496> or <#814459287308992532>.');
+				message.reply('Get general tech support in <#814459286460563496> or <#814459287308992532>.');
 			}
 		}
 		else if (args[0] === 'bot') {
-			message.channel.send('Work in progress...');
+			const botHelpEmbed = new Discord.MessageEmbed()
+				.setColor('#17a2b8')
+				.setTitle('Bot Commands:')
+				.addField('`!verify`', 'If you don\'t have the Members role, do this to verify yourself and get it')
+				.addField('`!help`', 'Get help with the server or bot.')
+				.addField('`!ping`', 'Check the bot latency and API latency');
+			message.channel.send(botHelpEmbed);
 		}
 	},
 };
