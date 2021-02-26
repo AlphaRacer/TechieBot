35daf1ccd867f9afcd4a614c4f54618642253d39
AlphaRacer <tarun.a1441@gmail.com>
2021-02-26 11:12:04 +0530

    fix bug in verify command

--- commands/verify.js
+++ commands/verify.js
@@ -1,14 +1,20 @@
 module.exports = {
-    name: 'verify',
-    description: 'verify command',
-    execute(message, args) {
-        try {
-            message.member.roles.add('814434340452827216');
-            message.client.channels.cache.get('814459282288279552').send(`${message.author}, welcome to the server!`);
-        }
-        catch (error) {
-            console.error(error);
-            message.reply("There was an error while running this command. Let AlphaRacer know about this so he will fix it!");
-        }
-    }
-}
+	name: 'verify',
+	description: 'verify command',
+	// eslint-disable-next-line no-unused-vars
+	execute(message, args) {
+		try {
+			if (message.guild.member.roles.has('814713329746444338')) {
+				message.reply('Nice try, you can\'t bypass mutes by doing this!');
+			}
+			else {
+				message.member.roles.add('814434340452827216');
+				message.client.channels.cache.get('814459282288279552').send(`${message.author}, welcome to the server!`);
+			}
+		}
+		catch (error) {
+			console.error(error);
+			message.reply('There was an error while running this command. Let AlphaRacer know about this so he will fix it!');
+		}
+	},
+};
