f75e7baacaa882df6666d27b64be641e7347d7be
AlphaRacer <tarun.a1441@gmail.com>
2021-02-26 12:13:03 +0530

    Update service command

--- commands/service.js
+++ commands/service.js
@@ -2,6 +2,9 @@ module.exports = {
 	name: 'service',
 	description: 'service command',
 	execute(message, args) {
+		if (!args[0]) {
+			message.reply('You have not given any arguments. \n\nTo view a Pack, send `!service <pack number>`, replace `<pack number>` with the pack number.\nTo use a Pack, send `!service use <pack number>`.');
+		}
 		switch(args[0]) {
 		case '1':
 			message.channel.send('Service 1: Server Moderation Bot Setup Pack');
@@ -19,21 +22,24 @@ module.exports = {
 			message.channel.send('Service 5: Full Server Setup Pack');
 			break;
 		case 'use':
+			if (!args[1]) {
+				message.reply('You have not provided the number of the pack you want to use!');
+			}
 			switch(args[1]) {
 			case '1':
-				message.reply('Successfully used Pack 1: Server Moderation Bot Setup Pack\n\nNow, DM AlphaRacer or AG with the invite link to the server where you want them to configure the bot. Note:- `You must be in https://discord.gg/UsaGyH4W3F or https://discord.gg/vKK5pbusRj to use a Pack.`');
+				message.reply('**Successfully used Pack 1: Server Moderation Bot Setup Pack**\n\nNow, DM AlphaRacer or AG with the invite link to the server where you want them to configure the bot. Note:- `You must be in https://discord.gg/UsaGyH4W3F or https://discord.gg/vKK5pbusRj to use a Pack.`');
 				break;
 			case '2':
-				message.reply('Successfully used Pack 2: Server Utility Bot Setup\n\nNow, DM AlphaRacer or AG with the invite link to the server where you want them to configure the bot. Note:- `You must be in https://discord.gg/UsaGyH4W3F or https://discord.gg/vKK5pbusRj to use a Pack.`');
+				message.reply('**Successfully used Pack 2: Server Utility Bot Setup**\n\nNow, DM AlphaRacer or AG with the invite link to the server where you want them to configure the bot. Note:- `You must be in https://discord.gg/UsaGyH4W3F or https://discord.gg/vKK5pbusRj to use a Pack.`');
 				break;
 			case '3':
-				message.reply('Successfully used Pack 3: Server Growth Pack\n\nNow, DM AlphaRacer or AG with the invite link to the server where you want them to configure the bot. Note:- `You must be in https://discord.gg/UsaGyH4W3F or https://discord.gg/vKK5pbusRj to use a Pack.`');
+				message.reply('**Successfully used Pack 3: Server Growth Pack**\n\nNow, DM AlphaRacer or AG with the invite link to the server where you want them to configure the bot. Note:- `You must be in https://discord.gg/UsaGyH4W3F or https://discord.gg/vKK5pbusRj to use a Pack.`');
 				break;
 			case '4':
-				message.reply('Successfully used Pack 4: Server Any Bot Setup Pack\n\nNow, DM AlphaRacer or AG with the invite link to the server where you want them to configure the bot. Note:- `You must be in https://discord.gg/UsaGyH4W3F or https://discord.gg/vKK5pbusRj to use a Pack.`');
+				message.reply('**Successfully used Pack 4: Server Any Bot Setup Pack**\n\nNow, DM AlphaRacer or AG with the invite link to the server where you want them to configure the bot. Note:- `You must be in https://discord.gg/UsaGyH4W3F or https://discord.gg/vKK5pbusRj to use a Pack.`');
 				break;
 			case '5':
-				message.reply('Successfully used Pack 5: Full Server Setup Pack\n\nNow, DM AlphaRacer or AG with the invite link to the server where you want them to configure the bot. Note:- `You must be in https://discord.gg/UsaGyH4W3F or https://discord.gg/vKK5pbusRj to use a Pack.`');
+				message.reply('**Successfully used Pack 5: Full Server Setup Pack**\n\nNow, DM AlphaRacer or AG with the invite link to the server where you want them to configure the bot. Note:- `You must be in https://discord.gg/UsaGyH4W3F or https://discord.gg/vKK5pbusRj to use a Pack.`');
 				break;
 			}
 			break;
